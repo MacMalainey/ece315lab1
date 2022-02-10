8e6c68100c927ce2e0c57155f182cf5a083329f7
username <celinefong4@gmail.com>
2022-02-10 01:52:34 -0700

    finished up most of part2, no idea if it works because i don't have a board :)

--- kypd_ssd_gpio.sdk/lab1proj2/src/freertos_hello_world.c
+++ kypd_ssd_gpio.sdk/lab1proj2/src/freertos_hello_world.c
@@ -1,35 +1,27 @@
+
 /*
  *  ECE- 315 WINTER 2021 - COMPUTER INTERFACING COURSE
+ *  Created on	: 	14 July, 2021
+ *      Author	: 	Shyama M. Gandhi, Mazen Elbaz
  *
- *  Created on	: 	15 July, 2021
- *  Author		: 	Shyama M. Gandhi, Mazen Elbaz
+ * --------------------------------------------------------------------------------
+ * IMPLEMENTATION OF A SINGLE DIGIT LOGICAL AND MODULO CALCULATOR.
  *
- * -----------------------------------------------------------------------------------------------------
- * IMPLEMENTATION OF A SIMPLE CALCULATOR.
- * Inputs Operands from the keypad
- * Output of the arithmetic operation is displayed on the Console
- * This exercise of the lab does not use SSD!!!
- * Operations available : +, -, * and palindrome, selected using the keys A, B, C and D, respectively.
+ * Inputs Operands from the keypad (such as using two operands op1=9, op2=3)
+ * Output of the operation is displayed on the Seven Segment (if one digit output, display it on right segment with left segment as '0'.
+ * Two digit output will be displayed on right and left side as well. Say, 9x9=81. '8' on left SSD and '1' on right SSD)
+ * Operations available : ^, |, & and %, selected using the push buttons on the board, 0001 for ^, 0010 for |, 0100 for &, 1000 for %
+ * Remember that x mod 0 is error so we display -1 on the SSD! (- on left SSD and 1 on the right SSD)
+ * Methodology for executing the application on board:
+ * You enter op1=9 from keypad, then press E, hold on the btn 0 for addition and keep it pressed. Now, enter the op2=8, press 'E'.
+ * btn=1 right now, output will be displayed on the ssd = 01 in this case.
  *
- * The design of this exercise is as follows:
- * Say, you wish to calculate (978 X 4050)
- * So, you enter 9, press 7, press 8 and then F so that the operand will be registered. Do the same for second operand of 4050.
- * In case while entering the operand, if you commit any error, you can press 'E' key any time to enter the operand again.
- * Once you have entered two operands, press any key from A, B, C or D to choose the corresponding operation.
- * So, the sequence of inputs you entered is, enter one operand, enter second operand and then enter the operation using the (A/B/C/D) key.
- * The calculator is designed in a way that you enter the operands first and then select the operation as a third value to the Queue.
- * The corresponding output will be displayed on the console.
- * 32-bit variables are used to store the input as well as output and overflow will generate a wrong output. You must detect the overflow condition for +, - and * operation.
- *
- * For subtraction, you may use store_operands[1]-store_operands[0] or vice versa.
- * For palindrome, two operands are taken as an input. Four results are possible: both operands are palindrome, operand 1 is palindrome but operand 2 is not. Operand 2 is palindrome but
- * operand 1 is not. Both operands are non-palindrome numbers. For any case, display the result on the console. However, for the case when both the operands are palindrome,
- * a White light on RGB led must glow for 1.5 seconds.
- * The initialization and required definitions for RGB led GPIO has been provided as you read along the template.
- * -----------------------------------------------------------------------------------------------------
+ * --------------------------------------------------------------------------------
+ * Please be aware that the initial template will not produce any output. Also, if you see an "error" on the line 195 where this is "if" statement, that is ok! Once you write the required code
+ * on this line, the syntax error will go away! You have to code the sections provided inside the comments. Carefully examine the comments during the
+ * course of the code so you don't miss anything.
  *
  */
-
 
 //Include FreeRTOS Library
 #include "FreeRTOS.h"
@@ -47,6 +39,13 @@
 #include "xil_cache.h"
 #include "math.h"
 
+// Parameter definitions
+#define BTNS_DEVICE_ID		XPAR_AXI_GPIO_BUTTONS_DEVICE_ID
+#define SSD_DEVICE_ID		XPAR_AXI_GPIO_PMOD_SSD_DEVICE_ID
+
+//Button Variable
+XGpio BTNInst, SSDInst;
+
 /* The Tx and Rx tasks as described at the top of this file. */
 static void prvTxTask( void *pvParameters );
 static void prvRxTask( void *pvParameters );
@@ -56,23 +55,35 @@ u32 SSD_decode(u8 key_value, u8 cathode);
 
 PmodKYPD myDevice;
 
+/*-----------------------------------------------------------*/
+
 static TaskHandle_t xTxTask;
 static TaskHandle_t xRxTask;
 static QueueHandle_t xQueue = NULL;
 
-//GPIO Variable RGBLED
-XGpio RGBInst;
-
 #define DEFAULT_KEYTABLE "0FED789C456B123A"
-
-//GPIO Parameter definitions from xparameters.h
-#define RGBLED_DEVICE_ID 	XPAR_AXI_GPIO_RGB_LED_DEVICE_ID
-
-#define WHITE_IN_RGB		7
 
 void DemoInitialize() {
    KYPD_begin(&myDevice, XPAR_AXI_GPIO_PMOD_KEYPAD_BASEADDR);
    KYPD_loadKeyTable(&myDevice, (u8*) DEFAULT_KEYTABLE);
+}
+
+//valid key press from 0-9 has already been encoded for you.
+u32 SSD_decode(u8 key_value, u8 cathode){
+
+	switch(key_value){
+	case 0: if(cathode==0) return 0b00111111; else return 0b10111111;
+	case 1: if(cathode==0) return 0b00000110; else return 0b10000110;
+	case 2: if(cathode==0) return 0b01011011; else return 0b11011011;
+	case 3: if(cathode==0) return 0b01001111; else return 0b11001111;
+	case 4: if(cathode==0) return 0b01100110; else return 0b11100110;
+	case 5: if(cathode==0) return 0b01101101; else return 0b11101101;
+	case 6: if(cathode==0) return 0b01111101; else return 0b11111101;
+	case 7: if(cathode==0) return 0b00000111; else return 0b10000111;
+	case 8: if(cathode==0) return 0b01111111; else return 0b11111111;
+	case 9: if(cathode==0) return 0b01101111; else return 0b11101111;
+	default:if(cathode==0) return 0b00000000; else return 0b00000000;
+	}
 }
 
 // MAIN FUNCTION
@@ -80,22 +91,32 @@ int main (void)
 {
   int status;
 
-  xil_printf("System Ready!\n");
-
-  // Initialize RGB LED
-  status = XGpio_Initialize(&RGBInst, RGBLED_DEVICE_ID);
+  // Initialize Push Buttons
+  status = XGpio_Initialize(&BTNInst, BTNS_DEVICE_ID);
   if(status != XST_SUCCESS){
-	  xil_printf("GPIO Initialization for RGB LED unsuccessful.\r\n");
+	  xil_printf("GPIO Initialization for BUTTONS unsuccessful.\r\n");
 	  return XST_FAILURE;
   }
 
-  // Set RGB LED direction to output
-  XGpio_SetDataDirection(&RGBInst, 1, 0x00);
+  // Initialize SSD
+  status = XGpio_Initialize(&SSDInst, SSD_DEVICE_ID);
+  if(status != XST_SUCCESS){
+    xil_printf("GPIO Initialization for SSD unsuccessful.\r\n");
+    return XST_FAILURE;
+  }
+
+  /*************************************/
+  //Set the directions of the buttons and SSD GPIO peripherals here
+  XGpio_SetDataDirection(&BTNInst, 0, 0xFF);
+  XGpio_SetDataDirection(&SSDInst, 1, 0x00);
+
+  /*************************************/
+
+  xil_printf("Initialization Complete, System Ready!\n");
 
   /* Create the two tasks.  The Tx task is given a higher priority than the
   Rx task. Dynamically changing the priority of Rx Task later on so the Rx task will leave the Blocked state and pre-empt the Tx
   task as soon as the Tx task fills the queue. */
-
   xTaskCreate( prvTxTask,					/* The function that implements the task. */
     			( const char * ) "Tx", 		/* Text name for the task, provided to assist debugging only. */
     			configMINIMAL_STACK_SIZE, 	/* The stack allocated to the task. */
@@ -110,8 +131,7 @@ int main (void)
     			tskIDLE_PRIORITY + 1,
     			&xRxTask );
 
-  /* Create the queue used by the tasks. */
-  xQueue = xQueueCreate( 3,				/* There are three items in the queue, two operands and then operation using keypad */
+  xQueue = xQueueCreate( 2,				/* There are only two spaces in the queue. */
 		  	  sizeof( unsigned int ) );	/* Each space in the queue is large enough to hold a uint32_t. */
 
   /* Check the queue was created. */
@@ -134,79 +154,77 @@ static void prvTxTask( void *pvParameters )
 	for( ;; ) {
 	   u16 keystate;
 	   XStatus status, last_status = KYPD_NO_KEY;
-	   u8 key, last_key = 'x';
-	   u32 factor = 0, current_value = 0;
+	   u8 key, last_key = 'x', store_key = 'x';
+	   u32 key_stroke_on_SSD=0;
 
+	   // Initial value of last_key cannot be contained in loaded KEYTABLE string
 	   Xil_Out32(myDevice.GPIO_addr, 0xF);
+
 	   xil_printf("PMOD KYPD demo started. Press any key on the Keypad.\r\n");
 
 	   uxPriority = uxTaskPriorityGet( NULL );
 
 	   while (1) {
 
-	      // Capture state of each key
-	      keystate = KYPD_getKeyStates(&myDevice);
+		   /*******************************************/
+		   //write one line of code to capture the state of each key here. Hint: use the keystate variable to store the output
+		   //Examine the function KYPD_getKeyStates() to implement it.
+		   /*******************************************/
+		   keystate = KYPD_getKeyStates(&myDevice);
 
 	      // Determine which single key is pressed, if any
 	      status = KYPD_getKeyPressed(&myDevice, keystate, &key);
 
-	      //this functions returns the number of entries in the queue, so when the queue is full, i.e., 2 entries, decreased the priority of this task and hence receive task
-	      //will immediately start to execute.
-	      if(uxQueueMessagesWaiting( xQueue ) == 3){
+	      if(uxQueueMessagesWaiting( xQueue ) == 2){
 
-			  /*********************************/
-	    	  // enter the function to dynamically change the priority when queue is full. This way when the queue is full here, we change the priority of this task
+	    	  /*********************************/
+	    	  // enter the function to dynamically change the priority when queue is full. This way when the queue is full here, we change the priority of this task.
 	    	  // and hence queue will be read in the receive task to perform the operation. If you change the priority here dynamically, make sure in the receive task to do the counter part!!!
 	    	  /*********************************/
+
+	    	  // sets the receive task to be higher than the current task
+	    	  vTaskPrioritySet(xRxTask, uxPriority + 1);
+
 	      }
 
 	      // Print key detect if a new key is pressed or if status has changed
 	      if (status == KYPD_SINGLE_KEY
 	            && (status != last_status || key != last_key)) {
-	         xil_printf("Key Pressed: %c\r\n", (char) key);
-	         last_key = key;
+				 xil_printf("Key Pressed: %c\r\n", (char) key);
+				 last_key = key;
 
-	         //whenever 'F' is pressed, the aggregated number will be registered as an operand
-	         if((char)key == 'F'){
-	        	 xil_printf("Final current_value of operand= %d\n",current_value);
+				 /***************************************/
+				 //Keys 'A', 'B', 'C', 'D' and 'F' will be ignored for this part of the lab 1. So, if user hits these keys, consider them invalid.
+				 //write the code to consider these key presses as invalid (use if condition) and prompt a message to user in this case
+				 //write the required code inside the "if" black box given below!!!
+				 /***************************************/
+				 if(key == 'A' || key == 'B' || key == 'C' || key == 'D' || key == 'F'){
+					 xil_printf("Invalid key pressed");
+				 }
+				 //case when we consider input key strokes from '0' to '9' (only these are the valid key inputs for arithmetic operation)
+				 else if((char)key != 'E' ){
+					 store_key = key;
+				 }
+				 //if user presses 'E' key, consider the last input key pressed as the operand
+				 else if((char)key == 'E'){
+				 	if(store_key != 'x'){
+						 xil_printf("Storing the operand %c to Queue...\n",(char) store_key);
+						 key_stroke_on_SSD = SSD_decode((int)store_key-48, 0);
+						 XGpio_DiscreteWrite(&SSDInst, 1, key_stroke_on_SSD);	//display the digit on SSD stored as an operand.
 
-	        	 /*******************************/
-	        	 //write the logic to enter the updated variable here to the Queue
-	        	 /*******************************/
+						 /****************************************/
+						 //Length of queue=2, hence we only store the key pressed value in queue, when 'E' will be pressed.
+						 //here you write the Queue function to store the value of the last key pressed before 'E'
+						 //hint: a variable is being used in this task that keeps the track of this key value (key presses before 'E')
+						 /****************************************/
 
-		         current_value = 0;
-	         }
-	         //if 'E' is pressed, it resets the current value of the operand and allows the user to enter a new value
-	         else if((char)key == 'E'){
-	        	 xil_printf("current_value of operand has been reset. Please enter the new value.\n");
-
-	        	 factor = 0;
-	        	 current_value = 0;
-	         }
-	         //case when we consider input key strokes from '0' to '9' (only these are the valid key inputs for all the four operations)
-			 //the current_value is aggregated as the user presses consecutive digits
-	         //e.g. if the user presses the following digits in this order 4 > 5 > 8  => current_value will end up being 458
-	         else if(key>='0' && key<='9'){
-	        	 factor = (int)(key - '0');
-	        	 current_value = current_value*10 + factor;
-	        	 xil_printf("current_value = %d\n",current_value);
-	         }
-	         else if((uxQueueMessagesWaiting( xQueue ) == 2) &&(
-					 (char)key == 'A' ||
-					 (char)key == 'B' ||
-					 (char)key == 'C' ||
-					 (char)key == 'D')){
-
-				 /*****************************************/
-	        	 //once two operands are in the queue, enter the third value to the queue to indicate the operation to be performed using A,B,C or D key
-	        	 //store the current key value to the queue as the third element
-	        	 /*****************************************/
-
-		         current_value = 0;
-	         }
+						 //TODO: how to know how much time to delay
+						 xQueueSendToBack(xQueue, &store_key, 0);
+					}
+				 }
 	      }
 	      else if (status == KYPD_MULTI_KEY && status != last_status)
-	         xil_printf("Error: Multiple keys pressed\r\n"); //this is valid whenever two or more keys are pressed together
+	    	  	  xil_printf("Error: Multiple keys pressed\r\n"); //this is valid whenever two or more keys are pressed together
 
 	      last_status = status;
 	      usleep(1000);
@@ -219,23 +237,89 @@ static void prvRxTask( void *pvParameters )
 	UBaseType_t uxPriority;
 	uxPriority = uxTaskPriorityGet( NULL );
 
-	const TickType_t xDelay1500ms = pdMS_TO_TICKS(1500UL);
-
 	for( ;; )
 	{
+		u32 store_operands[2];
+		int result=0;
+
+		unsigned int btn_value;
 
 		/***************************************/
-		//...Write code here to read the three elements from the queue and perform the required operation.
-		//...Display the output result on the console for all the four operations.
-		//...If you have dynamically changed the priority of this task in TxTask, you need to change the priority here accordingly, respectively using vTaskPrioritySet(). This can be done after
-		//	 you finish calculation part.
-		//...This way once the RxTask is done, TxTask will have a higher priority and hence will wait for the next series of inputs from the user
-		//...You can write a switch-case statement or if-else statements for each different operation
-		//...For the Palindrome check, think of a way to find the reverse of each operand (two loops for each operand!) Compared this reverse operand with the original operand.
-		//...For RGB led, look at the function that was used in previous labs for writing the value to the led. Initialization and color definition is already provided to you in this file.
+		//write the code to read the queue values which store two operands for the calculation here.
+		//You may use store_operands[] for doing that or your wish of variable can be used too.
 		/***************************************/
+		xQueueReceive(xQueue, store_operands, 0);
+		xQueueReceive(xQueue, store_operands + 1, 0);
+
+		/***************************************/
+		//read the btn value here to check what user has pressed (^/|/&/%) and store it in say "btn_value" variable declared in this task
+		//For btn(3:0) --> "1000" is for %, "0100" is for &, "0010" is for | and "0001" is for ^
+		/***************************************/
+		btn_value = XGpio_DiscreteRead(&BTNInst, 0);
+
+		//keep the button pressed for your choice of the arithmetic/logical operation
+		switch(btn_value){
+		case 1: result=store_operands[0]^store_operands[1]; break;
+		/*****************************************************************************************/
+		//add the remaining cases here
+		//you may also use the default case to display nothing or some prompt message saying that no operation selected by user.
+		//in the case when no operation is selected, exit this task and go back to the TxTask if you want.
+		//you may also wait here until the user selects any operation, later on perform the calculation and then go to the TxTask.
+		/*****************************************************************************************/
+		case 2: result=store_operands[0]|store_operands[1]; break;
+		case 4: result=store_operands[0]&store_operands[1]; break;
+		case 8: result=store_operands[0]%store_operands[1]; break;
+		default: {
+			xil_printf("No operation selected");
+
+			// go back to Txtask by setting this task's priority to be lower
+			vTaskPrioritySet( xTxTask, ( uxPriority + 1 ) );
+		}
+		}
+
+		xil_printf("Operation result = %d\n\n",result);
+
+		//Once the result is computed, we wish to display it on SSD
+		//the following logic is to extract the digits from the result. For example, 9x9=81 so we will first display 1 on right SSD and then 8 on the left SSD!
+		//please note that our operands are between 0 to 9 only. The result will never exceed a two-digit number in any case.
+
+		if(result<0)
+			xil_printf("Result is less than zero!!!\n");
 
 
+		vTaskDelay(pdMS_TO_TICKS(1500)); //this delay is merely to introduce the visual difference between the input operands and the output result!
+
+
+		//Compute MSB and LSB digits for 2-digit output
+		u8 lsb = result % 10;
+		u8 msb = result / 10;
+
+		u32 l_s_b = SSD_decode(lsb, 0);
+		u32 m_s_b = SSD_decode(msb, 1);
+
+		/**********************************************************************************/
+		//Use a for loop to display result for 100 Cycles (15ms + 15ms = 30 ms/loop iteration = output displayed for 3 seconds)
+		//for two digits, first a right segment is seen and then left segment is lit up.
+		//introduce a delay between left and the right side of display on SSD such that both segments seem to lit up simultaneously!
+		//If you found out the appropriate frequency value in the previous part1, that might help!
+		/**********************************************************************************/
+		// TODO: how to display result for only 100 Cycles
+		for( ; ; ) {
+			// lsb displayed on right side
+			XGpio_DiscreteWrite(&SSDInst, 1, l_s_b);
+			vTaskDelay(pdMS_TO_TICKS(16));
+
+			// msb displayed on left side
+			XGpio_DiscreteWrite(&SSDInst, 1, m_s_b);
+			vTaskDelay(pdMS_TO_TICKS(16));
+		}
+
+		//clear both the segments after the result is displayed.
+		XGpio_DiscreteWrite(&SSDInst, 1, 0b00000000);
+		XGpio_DiscreteWrite(&SSDInst, 1, 0b10000000);
+
+		//we are now done doing the calculation so again go back to the task 1 (TxTask) to get the new inputs!
+		vTaskPrioritySet( xTxTask, ( uxPriority + 1 ) );
 
 	}
 }
